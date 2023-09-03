01a3ee57c9d2a1c5383676aa6c1b752ffda9a482
code-with-rahul <contactwerahul@gmail.com>
2023-09-03 21:10:53 +0530

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <>
+    <h1 className='bg-cyan-400 px-4 py-4 text-center font-bold'>Welcome to our Ecommerce App</h1>
+    <p className='bg-cyan-400 px-5 py-5 text-center font-bold'>I ❤️ Web Development</p>
+    </>
+  )
+
+}
+
+export default index
+
